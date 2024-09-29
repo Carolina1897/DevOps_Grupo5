b3817e7c9028e29b2f4ff739787deea8f83f780e
luisajimenezherrera <luisa.jimenez5638@unaula.edu.co>
2024-09-28 22:35:19 -0500

    Añadiendo primeras clases de js sobre el proyecto

---
+++ clsFavoritos.js
@@ -0,0 +1,8 @@
+class clsFavoritos{
+    constructor(idFavoritos,idUsuario,idVehiculos,fechaagregado){
+        this.idFavoritos=idFavoritos;
+        this.idUsuario=idUsuario;
+        this.idVehiculos=idVehiculos;
+        this.fechaagregado=fechaagregado;
+    }
+}
---
+++ clsSeguros.js
@@ -0,0 +1,10 @@
+class clsSeguros {
+    constructor (idSeguro,IdVehiculos,tipoSeguro,fechainicio,fechafin,costo){
+        this.idSeguro=idSeguro;
+        this.IdVehiculos=IdVehiculos;
+        this.tipoSeguro=tipoSeguro;
+        this.fechainicio=fechainicio;
+        this.fechafin=fechafin;
+        this.costo=costo;
+    }
+}
