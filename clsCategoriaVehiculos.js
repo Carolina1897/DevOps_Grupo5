cb68f1f61ef48362810dabc9100958a9cbc2039a
luisajimenezherrera <luisa.jimenez5638@unaula.edu.co>
2024-10-06 23:57:21 -0500

    Agregada carpeta Autoconexion donde esta ya organizado en carpetas todos los archivos de la app. sus imagenes etc y eliminacion de los archivos que ya estan integrados

--- clsCategoriaVehiculos.js
+++
@@ -1,7 +0,0 @@
-class clsCategoriaVehiculos{
-    constructor(idCategoria,NombreCategoria,Descripcion){
-        this.idCategoria=idCategoria;
-        this.NombreCategoria=NombreCategoria;
-        this.Descripcion=Descripcion;
-    }
-}
