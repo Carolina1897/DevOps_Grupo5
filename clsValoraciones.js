8d2e5f31aedfce44df9a71bf225c720f9049bd65
luisajimenezherrera <143238608+luisajimenezherrera@users.noreply.github.com>
2024-10-07 16:37:57 -0500

    Eliminar archivos (#12)
    * Agregando archivos de las primeras clases de js para dar inicio al proyecto
    
    * Revert "Agregando archivos de las primeras clases de js para dar inicio al proyecto"
    
    This reverts commit 7eead44e172ec511f515d59a182559cafa2249d7.
    
    * Eliminados archivos innecesarios de la rama dev

--- clsValoraciones.js
+++
@@ -1,9 +0,0 @@
-class clsValoraciones {
-constructor(idValoraciones,idVendedor,idComprador,calificacion,comentario){
-    this.idValoraciones=idValoraciones;
-    this.idVendedor=idVendedor;
-    this.idComprador=idComprador;
-    this.calificacion=calificacion;
-    this.comentario=comentario;
-}
-}
