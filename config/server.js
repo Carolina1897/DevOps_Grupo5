2eea65c296cd497baa24ba56d46318d47cc8437f
Carolina1897 <143292934+Carolina1897@users.noreply.github.com>
2024-10-08 02:00:14 +0000

    Conexion base de datos

--- config/server.js
+++ config/server.js
@@ -15,10 +15,10 @@ const port = process.env.PORT || 3000;
 
 // Configuración de la conexión a la base de datos
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'luisa',
-    database: 'dboAutoConexion',
+    host: 'bkpnggnd0ehhmipfczuh-mysql.services.clever-cloud.com',
+    user: 'u5e29vsn1maqolo5',
+    password: '1C5HI3nzbgvp2INwfBRf',
+    database: 'bkpnggnd0ehhmipfczuh',
 };
 
 // Crear una instancia de la clase de conexión
